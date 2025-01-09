1fb3d348773245da2b5b9f0432c730a17085a956
Océane Bondon <oceanebondon30@gmail.com>
2025-01-09 17:11:32 +0100

    bla

--- mysql/MainWindow.xaml.cs
+++ mysql/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using System.Data;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
+using MySql.Data.MySqlClient;
 
 namespace mysql
 {
@@ -24,17 +18,22 @@ namespace mysql
 
         private void Connect()
         {
-            string connexionString = "server=localhost;database=test;uid=root;password=;";
+            string connexionString = "server=109.234.165.136;database=sc4judi6669_test;uid=sc4judi6669_oceane;password=E_*J$d(*F.Sh";
             MySqlConnection connexion = new MySqlConnection(connexionString);
+            MySqlCommand cmd = new MySqlCommand("SELECT name as NOM FROM users;", connexion);
+            DataTable dt = new DataTable();
+
             try
             {
                 connexion.Open();
-                MessageBox.Show("Bdd ouverte !");
+                dt.Load(cmd.ExecuteReader());
                 connexion.Close();
             }
             catch(Exception e ) {
                 throw new Exception("Erreur à l'ouverture de la bdd : " + e.Message);
             }
+
+            dg.ItemsSource = dt.DefaultView;
         }
     }
 }
